fef4e8027ff14251884823a13f6de8fc8e85152d
oskarmast <oskarmast9436@gmail.com>
2024-05-03 05:48:57 +0200

    fix: signup modal style issue

--- src/components/common/SignupModel.jsx
+++ src/components/common/SignupModel.jsx
@@ -21,6 +21,12 @@ const SignUpModal = () => {
             <div className="modal-close-btn" data-bs-dismiss="modal" />
             <div className="modal-header">
               <img src="/assets/img/home1/login-modal-header-img.jpg" alt="Sign Up" />
+              <span
+                className="modal-close-box"
+                onClick={toggleSignUpModal}
+              >
+                &times;
+              </span>
             </div>
             <div className="modal-body">
               <div className="login-registration-form">
@@ -31,24 +37,36 @@ const SignUpModal = () => {
                 <form>
                   <div className="form-inner mb-20">
                     <label className='label p-2'>
+                      <span className='text-base label-text'>First Name</span>
+                    </label>
+                    <input type="text" placeholder="Enter your First Name" className="input input-bordered" required/>
+                  </div>
+                  <div className="form-inner mb-20">
+                    <label className='label p-2'>
+                      <span className='text-base label-text'>Last Name</span>
+                    </label>
+                    <input type="text" placeholder="Create a password *" className="input input-bordered" required/>
+                  </div>
+                  <div className="form-inner mb-20">
+                    <label className='label p-2'>
                       <span className='text-base label-text'>Email address</span>
                     </label>
-                    <input type="email" placeholder="Enter your email address *" className="input input-bordered" />
+                    <input type="email" placeholder="Enter your email address *" className="input input-bordered" required/>
                   </div>
                   <div className="form-inner mb-20">
                     <label className='label p-2'>
                       <span className='text-base label-text'>Password</span>
                     </label>
-                    <input type="password" placeholder="Create a password *" className="input input-bordered" />
+                    <input type="password" placeholder="Create a password *" className="input input-bordered" required/>
                   </div>
                   <div className="form-inner mb-20">
                     <label className='label p-2'>
                       <span className='text-base label-text'>Confirm Password</span>
                     </label>
-                    <input type="password" placeholder="Confirm your password *" className="input input-bordered" />
+                    <input type="password" placeholder="Confirm your password *" className="input input-bordered" required/>
                   </div>
                   <div>
-                    <button type="button" className="login-btn mb-25">Sign Up</button>
+                    <button type="button" className="signup-modal-btn mb-25">Sign Up</button>
                   </div>
                   <a href='#'  data-bs-toggle="modal" data-bs-target="#user-login" className="login-link" onClick={login_modal_show}>
                     Already have an account? Sign in
