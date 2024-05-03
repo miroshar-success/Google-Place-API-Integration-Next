c1bc0959520edf328fde5d515d6d0abe07347151
oskarmast <oskarmast9436@gmail.com>
2024-05-03 16:07:54 +0200

    fix: message sending icon style

--- src/components/Messaging/MessageInput.jsx
+++ src/components/Messaging/MessageInput.jsx
@@ -1,22 +1,36 @@
-import Icon from '@/uitils/Icon'
-import React from 'react'
-
+import Icon from "@/uitils/Icon";
+import React from "react";
 
 const MessageInput = () => {
   return (
-    <form className='px-4 my-3'>
-        <div className='w-full relative'>
-          <input
-          type='text'
-          className='border text-sm rounded-lg block w-full p-2.5 bg-gray-500 border-gray-600 text-white'
-          placeholder='send a message'
-          />
-        </div>
-        <button type='submit' className='absolute inset-y-0 end-1 flex items-center pe-3'> 
-          <Icon name="email" width={20} height={14} viewBox='0 0 20 14'></Icon>
+    <form className="px-4 my-3">
+      <div className="w-full relative">
+        <input
+          type="text"
+          className="border text-sm rounded-lg block w-full p-2.5 bg-gray-500 border-gray-600 text-white"
+          placeholder="send a message"
+        />
+        <button
+          type="submit"
+          className="absolute inset-y-0 end-1 flex items-center pe-3"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 text-white"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <line x1="22" y1="2" x2="11" y2="13" />
+            <polygon points="22 2 15 22 11 13 2 9 22 2" />
+          </svg>
         </button>
+      </div>
     </form>
-  )
-}
+  );
+};
 
-export default MessageInput
+export default MessageInput;
